9aac6b43e845dc582ea676c2fcb4ac120291a7bd
Andre Augusto <andre.augusto@tecnico.ulisboa.pt>
2025-05-24 13:18:40 +0100

    fix(satp-hermes): get approve address param
    Signed-off-by: Andre Augusto <andre.augusto@tecnico.ulisboa.pt>

--- packages/cactus-plugin-satp-hermes/src/test/typescript/constants.ts
+++ packages/cactus-plugin-satp-hermes/src/test/typescript/constants.ts
@@ -1,2 +1,2 @@
 export const DOCKER_IMAGE_NAME = "aaugusto11/cacti-satp-hermes-gateway";
-export const DOCKER_IMAGE_VERSION = "deb9e0962-2025-05-23";
+export const DOCKER_IMAGE_VERSION = "c7e797eb1-2025-05-24";
