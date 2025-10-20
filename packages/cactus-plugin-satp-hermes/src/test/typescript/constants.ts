7790e9ca5896d80eb64190d6ed286814a96d3b0a
André Augusto <andre.augusto@tecnico.ulisboa.pt>
2025-10-20 20:03:22 +0100

    docker(satp-hermes): new docker image with extensions
    Signed-off-by: André Augusto <andre.augusto@tecnico.ulisboa.pt>

--- packages/cactus-plugin-satp-hermes/src/test/typescript/constants.ts
+++ packages/cactus-plugin-satp-hermes/src/test/typescript/constants.ts
@@ -1,3 +1,3 @@
 //export const SATP_DOCKER_IMAGE_NAME = "hyperledger/cacti-satp-hermes-gateway";
 export const SATP_DOCKER_IMAGE_NAME = "aaugusto11/cacti-satp-hermes-gateway";
-export const SATP_DOCKER_IMAGE_VERSION = "2025-10-20-dev-29d5a7719";
+export const SATP_DOCKER_IMAGE_VERSION = "2025-10-20-dev-19851825a";
